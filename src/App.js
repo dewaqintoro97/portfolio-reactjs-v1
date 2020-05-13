a96c13d60b67fde4ee954abb63fa79c02a033f0a
dewa qintoro <dewaqintoro@gmail.com>
2020-05-13 07:37:41 +0700

    part2 a&b fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 import Home from './components/index'
@@ -7,9 +8,9 @@ import Resume from './components/Resume';
 function App() {
   return (
     <>
-      <CssBaseline />
-        <Home/> 
-        <Resume/>
+      <CssBaseline />  
+        <Route exact path="/" component={Home}/>
+        <Route path="/resume" component={Resume}/>
     </>
   );
 }
