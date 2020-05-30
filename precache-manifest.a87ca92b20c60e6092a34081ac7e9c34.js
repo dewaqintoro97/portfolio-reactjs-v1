efc762af3960b99fdffde64b485030a4b3f4f263
dewaqintoro97 <dewa.qintoro@students.amikom.ac.id>
2020-05-31 03:09:44 +0700

    Updates

---
+++ precache-manifest.a87ca92b20c60e6092a34081ac7e9c34.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee556b13afcbeb77c31b82ca560bbbaf",
+    "url": "/portfolio-reactjs-v1/index.html"
+  },
+  {
+    "revision": "ef3762bd22ce7939e847",
+    "url": "/portfolio-reactjs-v1/static/css/main.8702d27e.chunk.css"
+  },
+  {
+    "revision": "2648149a7e22b15ef03f",
+    "url": "/portfolio-reactjs-v1/static/js/2.5c406574.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio-reactjs-v1/static/js/2.5c406574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3762bd22ce7939e847",
+    "url": "/portfolio-reactjs-v1/static/js/main.8adf6233.chunk.js"
+  },
+  {
+    "revision": "29d20050af6d93b8ff7d",
+    "url": "/portfolio-reactjs-v1/static/js/runtime-main.5caa7655.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio-reactjs-v1/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "7760aecd30d7c16aee836a495da8cd7a",
+    "url": "/portfolio-reactjs-v1/static/media/dew.7760aecd.png"
+  }
+]);
